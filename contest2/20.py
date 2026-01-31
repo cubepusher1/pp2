8c5a2dea22cd2a5c6a21dd03b1b02987c7a0d038
cubepusher1 <z_rakymzhan@kbtu.kz>
2026-01-31 20:12:12 +0500

    practice 2.4

--- contest2/20.py
+++ contest2/20.py
@@ -1,12 +1,31 @@
-n = int(input())
-dict = {}
+import sys
 
-for i in range(n):
-    inp = input().split()
-    if inp[0] == "set":
-        dict[inp[1]] = inp[2]
-    elif inp[0] == "get":
-        if inp[1] not in dict:
-            print("KE: no key", inp[1], "found in the document")
+data = sys.stdin.read().split()
+idx = 0
+n = int(data[idx])
+idx += 1
+
+d = {}
+
+out = []
+
+for _ in range(n):
+    cmd = data[idx]
+    idx += 1
+
+    if cmd == "set":
+        key = data[idx]
+        idx += 1
+        val = data[idx]
+        idx += 1
+        d[key] = val
+    elif cmd == "get":
+        key = data[idx]
+        idx += 1
+        if key in d:
+            out.append(d[key])
         else:
-            print(dict[inp[1]])
+            out.append(f"KE: no key {key} found in the document")
+
+# bulk output
+sys.stdout.write("\n".join(out))
