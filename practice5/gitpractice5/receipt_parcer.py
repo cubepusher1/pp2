e3803401798a8668393a28965accb47b9a594100
cubepusher1 <z_rakymzhan@kbtu.kz>
2026-03-07 14:05:58 +0500

    upd practice5

--- practice5/gitpractice5/receipt_parcer.py
+++ practice5/gitpractice5/receipt_parcer.py
@@ -7,8 +7,14 @@ pattern = re.compile(r"([A-Za-z ]+)\s+x(\d+)\s+\$(\d+\.\d{2})")
 
 matches = pattern.findall(text)
 
+total = 0
+
 if matches:
     for item, qty, price in matches:
-        print(f"Item: {item.strip()}, Quantity: {qty}, Price: ${price}, Total: ${int(qty)*float(price)}")
+        print(f"Item: {item.strip()}, Quantity: {qty}, Price: ${price}, Total: ${round(int(qty)*float(price), 2)}")
+        total += int(qty)*float(price)
 else:
     print("No items found.")
+
+if total != 0:
+    print(total)
