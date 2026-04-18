90e01df9e878b7fb01c8e31726403ce67dcb2691
cubepusher1 <z_rakymzhan@kbtu.kz>
2026-04-18 14:08:04 +0500

    practice8.1

--- practice8/init_db.py
+++ practice8/init_db.py
@@ -5,21 +5,18 @@ def run_sql_file(filename):
     if conn:
         try:
             cur = conn.cursor()
-            # Читаем твой SQL файл
             with open(filename, 'r', encoding='utf-8') as f:
                 sql = f.read()
             
-            # Выполняем всё содержимое
             cur.execute(sql)
             conn.commit()
-            print(f"✅ Файл {filename} успешно применен!")
+            print(f"{filename}")
         except Exception as e:
-            print(f"❌ Ошибка в файле {filename}: {e}")
+            print(f"error {filename}: {e}")
         finally:
             cur.close()
             conn.close()
 
 if __name__ == "__main__":
-    # Сначала создаем функции, потом процедуры
     run_sql_file("functions.sql")
     run_sql_file("procedures.sql")
