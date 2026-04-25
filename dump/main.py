5dd2a136560c8a91773a810dd83a006f4e722f05
cubepusher1 <z_rakymzhan@kbtu.kz>
2026-04-25 14:20:48 +0500

    practice10-11

--- dump/main.py
+++ dump/main.py
@@ -1,20 +1,4 @@
-import psycopg2
+matrix = []
+n = int(input())
 
-conn = psycopg2.connect(
-    dbname="postgres",
-    user="postgres",
-    host="127.0.0.1",
-    port="5432",
-    password="123456"
-)
-
-cursor = conn.cursor()
-
-cursor.execute("INSERT INTO people (name) VALUES (2, 'something')")
-cursor.execute("INSERT INTO people (name) VALUES (1, 'name')")
-
-conn.commit()
-print("SALAMALEKUM")
-
-cursor.close()
-conn.close()
+for i in range(n**2):
